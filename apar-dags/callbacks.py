eb6cf5d704483d3fc81abd8c88ca2686cb212dd4
Joel Simoneau <joel.simoneau@usherbrooke.ca>
2021-04-21 22:25:04 -0400

    bagwasim skeleton + callbacks

---
+++ apar-dags/callbacks.py
@@ -0,0 +1,38 @@
+from operators import ExtendedHttpOperator
+
+queries = dict()
+
+queries["jobUpdateStatus"] = """
+mutation jobUpdateStatus($jobId: ID!, $status: String!) {
+    jobUpdateStatus(input: { jobId: $jobId, status: $status }) {
+        job {
+            id
+            status
+        }
+    }
+}
+"""
+
+
+def get_job_status_update(status, **context):
+    job_id = context["dag_run"].conf.get("job_id")
+
+    return json.dumps({
+        "query": queries["jobUpdateStatus"],
+        "variables": {
+            "jobId": job_id,
+            "status": status
+        }
+    })
+
+
+def callback_factory(dag, task_id, status):
+    return ExtendedHttpOperator(
+        http_conn_id="apar_graphql",
+        endpoint="graphql/",
+        method="POST",
+        headers={"Content-Type": "application/json"},
+        data_fn=partial(get_job_status_update, status),
+        task_id=task_id,
+        dag=dag
+    )
---
+++ apar-dags/dag_bacgwasim.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+
+from callbacks import callback_factory
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime.utcnow(),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2)
+}
+
+dag = DAG(
+    "bacgwasim",
+    default_args=default_args,
+    schedule_interval=None,
+)
+
+# Callbacks
+start_callback = callback_factory(dag, "start_callback", "RUNNING")
+completed_callback = callback_factory(dag, "completed_callback", "COMPLETED")
+failed_callback = callback_factory(dag, "failed_callback", "FAILED")
+
+passing = KubernetesPodOperator(
+    namespace="airflow",
+    image="python:3.6",
+    cmds=["python","-c"],
+    arguments=["print('hello world')"],
+    labels={"foo": "bar"},
+    name="passing-test",
+    task_id="passing-task",
+    get_logs=True,
+    dag=dag
+)
+
+with dag:
+    start_callback >> passing >> [completed_callback, failed_callback]
