9673ff61aa1dc091c38f811e52f3c499b893bcca
Joel Simoneau <joel.simoneau@usherbrooke.ca>
2021-05-26 11:02:30 -0400

    eval in k8s operator exec

--- dags/operators/ContextualizedKubernetesPodOperator.py
+++ dags/operators/ContextualizedKubernetesPodOperator.py
@@ -1,8 +1,11 @@
+from typing import Optional
+
+from airflow.exceptions import AirflowException
+from airflow.kubernetes import kube_client
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.utils import pod_launcher
 from airflow.utils.decorators import apply_defaults
-import jinja2
-from kubernetes.client import CoreV1Api, models as k8s
-from typing import Any, Dict
+from airflow.utils.state import State
 
 
 class ContextualizedKubernetesPodOperator(KubernetesPodOperator):
@@ -13,21 +16,52 @@ class ContextualizedKubernetesPodOperator(KubernetesPodOperator):
         super(ContextualizedKubernetesPodOperator, self).__init__(*args, **kwargs)
 
 
-    def _render_nested_template_fields(
-        self,
-        content: Any,
-        context: Dict,
-        jinja_env: "jinja2.NativeEnvironment",
-        seen_oids: set,
-    ) -> None:
-        if id(content) not in seen_oids and isinstance(content, k8s.V1EnvVar):
-            seen_oids.add(id(content))
-            self._do_render_template_fields(content, ('value', 'name'), context, jinja_env, seen_oids)
-            return
+    def execute(self, context) -> Optional[str]:
+        try:
+            if self.in_cluster is not None:
+                client = kube_client.get_kube_client(
+                    in_cluster=self.in_cluster,
+                    cluster_context=self.cluster_context,
+                    config_file=self.config_file,
+                )
+            else:
+                client = kube_client.get_kube_client(
+                    cluster_context=self.cluster_context, config_file=self.config_file
+                )
 
-        super()._render_nested_template_fields(
-            content,
-            context,
-            jinja_env,
-            seen_oids
-        )
+            self.arguments = eval(self.arguments)
+            self.pod = self.create_pod_request_obj()
+            self.namespace = self.pod.metadata.namespace
+
+            self.client = client
+
+            # Add combination of labels to uniquely identify a running pod
+            labels = self.create_labels_for_pod(context)
+
+            label_selector = self._get_pod_identifying_label_string(labels)
+
+            pod_list = client.list_namespaced_pod(self.namespace, label_selector=label_selector)
+
+            if len(pod_list.items) > 1 and self.reattach_on_restart:
+                raise AirflowException(
+                    f'More than one pod running with labels: {label_selector}'
+                )
+
+            launcher = pod_launcher.PodLauncher(kube_client=client, extract_xcom=self.do_xcom_push)
+
+            if len(pod_list.items) == 1:
+                try_numbers_match = self._try_numbers_match(context, pod_list.items[0])
+                final_state, result = self.handle_pod_overlap(
+                    labels, try_numbers_match, launcher, pod_list.items[0]
+                )
+            else:
+                self.log.info("creating pod with labels %s and launcher %s", labels, launcher)
+                final_state, _, result = self.create_new_pod_for_operator(labels, launcher)
+            if final_state != State.SUCCESS:
+                status = self.client.read_namespaced_pod(self.pod.metadata.name, self.namespace)
+                raise AirflowException(f'Pod {self.pod.metadata.name} returned a failure: {status}')
+            context['task_instance'].xcom_push(key='pod_name', value=self.pod.metadata.name)
+            context['task_instance'].xcom_push(key='pod_namespace', value=self.namespace)
+            return result
+        except AirflowException as ex:
+            raise AirflowException(f'Pod Launching failed: {ex}')
