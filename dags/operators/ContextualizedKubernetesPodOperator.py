983a71fccfb693043414b88e31ee2f5a4e1a3c98
Joel Simoneau <joel.simoneau@usherbrooke.ca>
2021-05-26 10:22:59 -0400

    jinja NativeEnv

--- dags/operators/ContextualizedKubernetesPodOperator.py
+++ dags/operators/ContextualizedKubernetesPodOperator.py
@@ -7,3 +7,23 @@ class ContextualizedKubernetesPodOperator(KubernetesPodOperator):
     def __init__(self,
                  *args, **kwargs):
         super(ContextualizedKubernetesPodOperator, self).__init__(*args, **kwargs)
+
+
+    def _render_nested_template_fields(
+        self,
+        content: Any,
+        context: Dict,
+        jinja_env: "jinja2.NativeEnvironment",
+        seen_oids: set,
+    ) -> None:
+        if id(content) not in seen_oids and isinstance(content, k8s.V1EnvVar):
+            seen_oids.add(id(content))
+            self._do_render_template_fields(content, ('value', 'name'), context, jinja_env, seen_oids)
+            return
+
+        super()._render_nested_template_fields(
+            content,
+            context,
+            jinja_env,
+            seen_oids
+        )
